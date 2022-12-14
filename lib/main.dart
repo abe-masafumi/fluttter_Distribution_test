24883b6ed4db80fc3cf1c59af921b2ca117a2515
M1-Masa <masa455.wv@gmail.com>
2022-12-15 01:03:25 +0900

    commit test

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -115,7 +119,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
