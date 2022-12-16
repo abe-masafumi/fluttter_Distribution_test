764e4c72c0a3b6484cf9f8b5e4904e8ea0b61f72
M1-Masa <masa455.wv@gmail.com>
2022-12-16 20:39:51 +0900

    add: flutter analyzeの問題を解決

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
+// ignore: depend_on_referenced_packages
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
